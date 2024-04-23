a57f680c09e5e53a1165ae2ea03665d970e67f81
donioscode <doniyornormuxammedov@gmail.com>
2024-04-23 16:52:57 +0500

    firs added 2 fiel

---
+++ Destini-App-MVC/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini-App-MVC
+//
+//  Created by doniyor normuxammedov on 23/04/24.
+//
+
+import Foundation
---
+++ Destini-App-MVC/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini-App-MVC
+//
+//  Created by doniyor normuxammedov on 23/04/24.
+//
+
+import Foundation
