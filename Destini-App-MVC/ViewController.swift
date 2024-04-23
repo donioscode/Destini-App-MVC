97decbcdac99aa11f3b3428bc5442764f5fceec3
donioscode <doniyornormuxammedov@gmail.com>
2024-04-23 16:49:34 +0500

    Initial Commit

---
+++ Destini-App-MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini-App-MVC
+//
+//  Created by doniyor normuxammedov on 23/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
