b8dd4eaece834fb8c49ab65e57691cb8260bf0ac
donioscode <doniyornormuxammedov@gmail.com>
2024-04-24 13:59:17 +0500

    added UI IBOutlets and btn Action and model example data

--- Destini-App-MVC/Controller/ViewController.swift
+++ Destini-App-MVC/Controller/ViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLB: UILabel!
+    @IBOutlet weak var choice1Btn: UIButton!
+    @IBOutlet weak var choice2Btn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
     }
-    */
-
 }
